4fbd2c559085504518e324a54b6b853cc89930bb
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-30 11:14:37 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 const choices = document.querySelectorAll('button')
 const userChoice = document.getElementById('user-choice')
 const computerChoice = document.getElementById('computer-choice')
+const result = document.getElementById('result')
 let selectedChoice
+let generatedComputerChoice
+let gameResult
 
 choices.forEach(choice => choice.addEventListener('click', (e) => {
     selectedChoice = e.target.id
     userChoice.innerHTML = selectedChoice
     generateComputerChoice()
+    gerResults()
 }))
 
 function generateComputerChoice() {
@@ -19,5 +23,20 @@ function generateComputerChoice() {
     } else {
         generatedComputerChoice = 'scissors'
     }
+
     computerChoice.innerHTML = generatedComputerChoice
+}
+
+function gerResults() {
+    if (generatedComputerChoice === selectedChoice) {
+        gameResult = "It's a draw"
+    } else if (generatedComputerChoice === 'rock' && selectedChoice === 'paper' ||
+    generatedComputerChoice === 'paper' && selectedChoice === 'scissors' ||
+    generatedComputerChoice === 'scissors' && selectedChoice === 'rock'
+    ) {
+        gameResult = "You won 🥳"
+    } else {
+        gameResult = "You lost 😔"
+    }
+    result.innerHTML = gameResult
 }
