8449b670de5635dbcd444fd89f9fae4bfed008f6
Lu-Smith <104765947+Lu-Smith@users.noreply.github.com>
2022-09-30 10:55:33 +0100

    Updated script.js

--- script.js
+++ script.js
@@ -5,5 +5,10 @@ let selectedChoice
 choices.forEach(choice => choice.addEventListener('click', (e) => {
     selectedChoice = e.target.id
     userChoice.innerHTML = selectedChoice
-
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+    console.log(randomNumber)
+}
